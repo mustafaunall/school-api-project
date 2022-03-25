6f53ec6b0fe10240e44f8c1b5a88490574b86389
MustafaUnal <mustafaunaldev@gmail.com>
2022-03-25 15:29:44 +0300

    added users and user/id routes

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require('express')
 const app = express()
+const axios = require('axios')
 
 require('dotenv').config()
 const { PORT } = process.env
 const appVersion = require('./package.json').version
+
+const getUsers = (userId = null) => new Promise((resolve, reject) => {
+    let url = `https://jsonplaceholder.typicode.com/users`
+    if (userId) url += `/${userId}`
+    axios.get(url)
+    .then(res => {
+        resolve(res.data)
+    })
+    .catch(e => reject(e))
+})
+
+app.get('/users', async (req, res) => {
+    let data = await getUsers()
+    res.status(200).send(data)
+})
+
+app.get('/user/:userId', async (req, res) => {
+    let { userId } = req.params
+    let data = await getUsers(userId)
+    res.status(200).send(data)
+})
 
 app.get('/version', (req, res) => {
     res.status(200).json({
