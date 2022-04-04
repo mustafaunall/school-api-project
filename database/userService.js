50863185057524ef1e268ff909eba3611fc09923
MustafaUnal <mustafaunaldev@gmail.com>
2022-04-04 11:44:24 +0300

    updated

--- database/userService.js
+++ database/userService.js
@@ -2,11 +2,16 @@ const { MongoClient } = require('mongodb')
 
 require('dotenv').config()
 const { MONGO_URI, DB_NAME } = process.env
-const dbClient = new MongoClient(MONGO_URI)
 
-const Get = async (...filter) => {
-    return new Promise((resolve, reject) => {
-        await dbClient.connect()
-        const users = dbClient.db(DB_NAME).collection('users').findOne({ ...filter })
-    })
+class UserService {
+    dbClient = new MongoClient(MONGO_URI)
+
+    Get = async (...filter) => {
+        return new Promise((resolve, reject) => {
+            await dbClient.connect()
+            const users = dbClient.db(DB_NAME).collection('users').findOne({ ...filter })
+        })
+    }
 }
+
+module.exports = UserService
